e3d318ff6678c9b6961c3fb8f6165f54c18f6b4f
zhanz1 <52364576+zhanz1@users.noreply.github.com>
2020-05-13 22:15:41 -0500

    uploadData fix

--- corona/myRE.py
+++ corona/myRE.py
@@ -14,6 +14,45 @@ import pandas as pd
 from myClasses import MyLogging
 
 
+def goLog(
+        log_path, script, level='info', mode='w', max_size=10000000):
+    """
+    This function will help other program to log what they did
+    or error messages. 
+    """
+    if not os.path.exists(log_path):
+        raise FileNotFoundError('Path does not exist.')
+
+    try:
+        os.makedirs(log_path + 'log/')
+    except FileExistsError:
+        mode = 'a'
+    log_path = log_path + 'log/'
+
+    list_file = list(filter(lambda file: file.endswith(
+        '.log'), os.listdir(log_path)))
+    if len(list_file) == 0:
+        file_name = 'AWS.ZHANZ1.DATA_log_01.log'
+    else:
+        last_file = list_file[len(list_file) - 1]
+        if os.path.getsize(log_path + last_file) < max_size:
+            file_name = last_file
+        else:
+            current_num1 = int(last_file[20])
+            current_num2 = int(last_file[21])
+            if (current_num2 + 1) == 10:
+                current_num1 += 1
+                current_num2 = 0
+            else:
+                current_num2 += 1
+            num = str(current_num1) + str(current_num2)
+            file_name = 'AWS.ZHANZ1.DATA_log_' + num + '.log'
+
+    MyLog = MyLogging()
+    MyLog.basicConfig(filename=log_path+file_name, filemode=mode, level=20)
+    MyLog.log(level, script)
+
+
 def getDate():
     """
     This function will return a string with
@@ -68,28 +107,23 @@ def prepareData(input_list):
         return output + "'" + input_list[len(input_list) - 1] + "'"
 
 
-connection = psycopg2.connect(user='postgres',
-                              password='qwertyuiop135',
-                              host='127.0.0.1',
-                              port=5000,
-                              database='postgres')
-cursor = connection.cursor()
-cursor.execute(
-    "select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';")
-f = cursor.fetchall()
-print([name for tuples in f for name in tuples])
-
-def uploadData(upload_file, port, table_name='default', log_path='C:/Users/zhan1/Desktop/Python/project_jack/corona/', addData=False):
+def uploadData(upload_file, port, table_name='default', log_path='C:/Users/zhan1/Desktop/Python/project_jack/corona/'):
     try:
-        connection = psycopg2.connect(user='postgres', 
-                                            password='qwertyuiop135', 
-                                            host='127.0.0.1', 
-                                            port=port,
-                                            database='postgres')
-        cursor = connection.cursor()
+        psycopg2.connect(user='postgres', 
+                        password='qwertyuiop135', 
+                        host='127.0.0.1', 
+                        port=port,
+                        database='postgres')
     except:
         goLog(log_path, 'PostgresSQL_uploadData: Failed to connect to PSQL', level='error')
         quit
+    
+    connection = psycopg2.connect(user='postgres',
+                                  password='qwertyuiop135',
+                                  host='127.0.0.1',
+                                  port=port,
+                                  database='postgres')
+    cursor = connection.cursor()
 
     if upload_file.endswith('csv'):
         try:
@@ -101,33 +135,22 @@ def uploadData(upload_file, port, table_name='default', log_path='C:/Users/zhan1
         columns = lines[0].replace('/', '_').replace(' ', '_').lower()
         columns_list = columns.split(',')
 
-        if addData and table_name != 'default':
-            cursor.execute("SELECT * FROM " + table_name)
-            existing_column = [desc[0] for desc in cursor.description]
+        table_name = 'corona_data_' + getDate().replace('-', '_')
+        create_table_query = 'CREATE TABLE ' + table_name + ' ('
+        for column in columns_list:
+            create_table_query += column + ' text NOT NULL,' 
 
-            if not all(elem in columns_list for elem in existing_column):
-                column_query = 'ALTER TABLE ' + table_name
-                for column in columns_list:
-                    if column not in existing_column:
-                        column_query += ' ADD COLUMN ' + column + ' text,'
-                column_query = column_query[:-1] + ';'
-                try:
-                    cursor.execute(column_query)
-                except:
-                    goLog(log_path, 'PostgresSQL_uploadData: Failed to add to table', level='error')
-                    quit
+        create_table_query = create_table_query[:-1] + ');'
 
-        elif table_name == 'default' and addData:
-            raise KeyError('Have to specify the table if you want to add data.')
+        try:
+            cursor.execute(create_table_query)
+        except:
+            goLog(
+                log_path, 'PostgresSQL_uploadData: Failed to create table.', level='error')
+            quit
 
-        elif table_name == 'default':
-            table_name = 'corona_data_' + getDate().replace('-', '_')
-            create_table_query = 'CREATE TABLE ' + table_name + ' ()'
-            try:
-                cursor.execute(create_table_query)
-            except:
-                goLog(log_path, 'PostgresSQL_uploadData: Failed to create table.', level='error')
-                quit
+        cursor.execute('SELECT * FROM ' + table_name)
+        existing_column = [desc[0] for desc in cursor.description]
 
         datas = lines[1:]
         add_data_query = 'INSERT INTO ' + table_name + ' (' + columns + ')' + ' VALUES '
@@ -145,8 +168,27 @@ def uploadData(upload_file, port, table_name='default', log_path='C:/Users/zhan1
         add_data_query = 'INSERT INTO ' + table_name + ' (date, img) VALUES (' + date + ', ' + str(psycopg2.Binary(data)) + ')'
         cursor.execute(add_data_query)
 
+    goLog(log_path, 'PostgresSQL_uploadData: Successfully uploaded data to Database')
     connection.commit()  
     connection.close()
+
+
+uploadData(
+    'C:/Users/zhan1/Desktop/Python/project_jack/corona/2020-05-09-coronavirus.csv', 5000)
+
+"""
+connection = psycopg2.connect(user='postgres',
+                              password='qwertyuiop135',
+                              host='127.0.0.1',
+                              port=5000,
+                              database='postgres')
+cursor = connection.cursor()
+cursor.execute(
+    "select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';")
+f = cursor.fetchall()
+print([name for tuples in f for name in tuples])
+connection.close()
+"""
 
 
 def generateGraph(input_csv, columns=['Country/Other', 'Total Cases'], upload=False):
@@ -176,42 +218,6 @@ def getHTML(file_path):
     return html
 
 
-def goLog(
-    log_path, script, level='info', mode='w', max_size=10000000):
-    """
-    This function will help other program to log what they did
-    or error messages. 
-    """
-    if not os.path.exists(log_path):
-        raise FileNotFoundError('Path does not exist.')
 
-    try:
-        os.makedirs(log_path + 'log/')
-    except FileExistsError:
-        mode = 'a'
-    log_path = log_path + 'log/'
-
-    list_file = list(filter(lambda file: file.endswith(
-        '.log'), os.listdir(log_path)))
-    if len(list_file) == 0:
-        file_name = 'AWS.ZHANZ1.DATA_log_01.log'
-    else:
-        last_file = list_file[len(list_file) - 1]
-        if os.path.getsize(log_path + last_file) < max_size:
-            file_name = last_file
-        else:
-            current_num1 = int(last_file[20])
-            current_num2 = int(last_file[21])
-            if (current_num2 + 1) == 10:
-                current_num1 += 1
-                current_num2 = 0
-            else:
-                current_num2 += 1
-            num = str(current_num1) + str(current_num2)
-            file_name = 'AWS.ZHANZ1.DATA_log_' + num + '.log'
-    
-    MyLog = MyLogging()
-    MyLog.basicConfig(filename=log_path+file_name, filemode=mode, level=20)
-    MyLog.log(level, script)
 
 
